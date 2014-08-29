ee7c55b5922a800c2225919b377ebd8ec1cfb1b4
karim hamidou <khamidou@gmail.com>
2014-08-29 19:10:56 +0200

    changed folders order

--- inbox/__init__.py
+++ inbox/__init__.py
@@ -1,6 +1,8 @@
 from .client import APIClient
+from .client.client import APIClient
 from pkgutil import extend_path
 
 # Allow out-of-tree submodules.
 __path__ = extend_path(__path__, __name__)
+
 __all__ = ['APIClient']
