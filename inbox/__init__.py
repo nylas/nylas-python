fd3bf464d81376d9f27d8a19ad891c6546841449
karim hamidou <khamidou@gmail.com>
2014-08-29 19:29:01 +0200

    actually, a better path

--- inbox/__init__.py
+++ inbox/__init__.py
@@ -1,4 +1,6 @@
 from pkgutil import extend_path
+from .client import APIClient
 
 # Allow out-of-tree submodules.
 __path__ = extend_path(__path__, __name__)
+__all__ = ['APIClient']
--- inbox/client/__init__.py
+++ inbox/client/__init__.py
@@ -1,5 +1,3 @@
 from pkgutil import extend_path
-from .client import APIClient
 
 __path__ = extend_path(__path__, __name__)
-__all__ = ['APIClient']
