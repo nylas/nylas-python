0dcdd01e0614a0de7789ec1d7ef891b32c27bf04
karim hamidou <khamidou@gmail.com>
2014-08-29 19:21:32 +0200

    cleaner package

--- inbox/__init__.py
+++ inbox/__init__.py
@@ -1,8 +1,4 @@
-from .client import APIClient
-from .client.client import APIClient
 from pkgutil import extend_path
 
 # Allow out-of-tree submodules.
 __path__ = extend_path(__path__, __name__)
-
-__all__ = ['APIClient']
