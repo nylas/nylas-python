8125a9f6ba0ebd64b313841f63adb48e4e3df4a8
David Baumgold <david@davidbaumgold.com>
2017-07-17 10:02:51 -0400

    invalid file upload

--- tests/test_files.py
+++ tests/test_files.py
@@ -19,6 +19,17 @@ def test_file_upload(api_client):
     assert data == 'test body'
 
 
+def test_file_invalid_upload(api_client):
+    myfile = api_client.files.create()
+    with pytest.raises(FileUploadError) as exc:
+        myfile.save()
+
+    assert exc.value.message == (
+        "File object not properly formatted, "
+        "must provide either a stream or data."
+    )
+
+
 def test_file_upload_errors(api_client):
     myfile = api_client.files.create()
     myfile.filename = 'test.txt'
