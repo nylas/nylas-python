8dc46faa9b63c1f74ded99dc4d44ce39ca05e579
David Baumgold <david@davidbaumgold.com>
2017-07-17 10:45:17 -0400

    More account tests

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,3 +1,4 @@
+import pytest
 from nylas.client.restful_models import Account, APIAccount
 
 
@@ -11,3 +12,28 @@ def test_create_apiaccount(api_client, monkeypatch):
     monkeypatch.setattr(api_client, "is_opensource_api", lambda: True)
     account = api_client.accounts.create()
     assert isinstance(account, APIAccount)
+
+
+def test_account_json(api_client, monkeypatch):
+    monkeypatch.setattr(api_client, "is_opensource_api", lambda: False)
+    account = api_client.accounts.create()
+    result = account.as_json()
+    assert isinstance(result, dict)
+
+
+@pytest.mark.xfail
+def test_account_upgrade(api_client, monkeypatch):
+    monkeypatch.setattr(api_client, "is_opensource_api", lambda: False)
+    account = api_client.accounts.create()
+    assert account.billing_state is False  # what should this be?
+    account.upgrade()
+    assert account.billing_state is True
+    account.downgrade()
+    assert account.billing_state is False
+
+
+def test_account_delete(api_client, monkeypatch):
+    monkeypatch.setattr(api_client, "is_opensource_api", lambda: False)
+    account = api_client.accounts.create()
+    with pytest.raises(NotImplementedError):
+        account.delete()
