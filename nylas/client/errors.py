955b6b4058a6ba63dc8511f32af0413ccc39f15e
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2021-11-04 17:39:20 -0400

    Added a new error class
    This error class is an extension of `HTTPError` which we used to throw, and provides the error message and type from the server for the user to see

--- nylas/client/errors.py
+++ nylas/client/errors.py
@@ -1,3 +1,8 @@
+import json
+
+from requests import HTTPError
+
+
 class NylasError(Exception):
     pass
 
@@ -17,3 +22,22 @@ class UnSyncedError(NylasError):
     """
 
     pass
+
+
+class NylasApiError(HTTPError):
+    """
+    Error class for Nylas API Errors
+    This class provides more information to the user sent from the server, if present
+    """
+    def __init__(self, response):
+        response_json = json.loads(response.text)
+        if response_json and "message" in response_json and "type" in response_json:
+            error_message = u"%s %s. Reason: %s. Nylas Error Type: %s" % (
+                response.status_code,
+                response.reason,
+                response_json["message"],
+                response_json["type"],
+            )
+            super(NylasApiError, self).__init__(error_message, response=response)
+        else:
+            super(NylasApiError, self).__init__(response.text, response=response)
