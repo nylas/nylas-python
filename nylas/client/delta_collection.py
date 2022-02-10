704f2139c50a774f6f2be6c8e39d39bc2b9626c5
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2022-02-10 18:59:15 -0500

    Add `latest_cursor` and `since` support

--- nylas/client/delta_collection.py
+++ nylas/client/delta_collection.py
@@ -1,6 +1,44 @@
+from nylas.client.delta_models import Delta, Deltas
+
+
 class DeltaCollection:
     path = "delta"
 
     def __init__(self, api):
         self.api = api
 
+    def latest_cursor(self):
+        """
+        Returns the latest delta cursor
+
+        Returns:
+            str: The latest cursor
+
+        Raises:
+            RuntimeError: If the server returns an object without a cursor
+        """
+        response = self.api._post_resource(
+            Delta, "latest_cursor", None, None, path=self.path
+        )
+        if "cursor" not in response:
+            raise RuntimeError(
+                "Unexpected response from the API server. Returned 200 but no 'cursor' string found."
+            )
+
+        return response["cursor"]
+
+    def since(self, cursor):
+        """
+        Get a list of delta cursors since a specified cursor
+
+        Args:
+            cursor (str): The first cursor to request from
+
+        Returns:
+            Deltas: String for writing directly into an ICS file
+        """
+
+        response = self.api._get_resource_raw(
+            Delta, None, path=self.path, cursor=cursor
+        ).json()
+        return Deltas.create(self.api, **response)
