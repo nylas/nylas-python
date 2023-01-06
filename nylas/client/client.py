fdbfeb976adcd323b71158cc92963bd98444909c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-01-06 11:53:36 -0500

    Return dict not response object

--- nylas/client/client.py
+++ nylas/client/client.py
@@ -231,7 +231,7 @@ class APIClient(json.JSONEncoder):
         results = _validate(resp).json()
 
         self.access_token = results["access_token"]
-        return resp
+        return results
 
     def token_for_code(self, code):
         """
