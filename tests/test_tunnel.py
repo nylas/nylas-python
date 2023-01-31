7d83cd95d66210e23e4bef2a687addc34ea197d7
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-01-31 15:54:16 -0500

    Fix relative imports

--- tests/test_tunnel.py
+++ tests/test_tunnel.py
@@ -2,13 +2,14 @@ import json
 
 import pytest
 from urlobject import URLObject
-import services.tunnel
-from client.restful_models import Webhook
+
+from nylas.services import tunnel
+from nylas.client.restful_models import Webhook
 
 
 @pytest.mark.usefixtures("mock_create_webhook")
 def test_register_webhook(mocked_responses, api_client_with_client_id):
-    services.tunnel._register_webhook(
+    tunnel._register_webhook(
         api_client_with_client_id,
         "domain.com",
         "tunnel_id",
