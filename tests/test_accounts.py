808000d4e91c29e9109d09058f8dbaae6d4c71ba
Kiran Raju <kiran.raju@nylas.com@Kirans-MacBook-Pro-2.local>
2023-03-21 15:52:17 -0500

    Adding optional parameter for token-info endpoint

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -37,6 +37,13 @@ def test_token_info(api_client_with_client_id):
     assert "updated_at" in result
     assert "scopes" in result
 
+@pytest.mark.usefixtures("mock_token_info", "mock_account")
+def test_token_info(api_client_with_client_id):
+    result = api_client_with_client_id.token_info(account_id="anvkhwelkfdoehdflhdjkfhe1")
+    assert isinstance(result, dict)
+    assert "updated_at" in result
+    assert "scopes" in result
+
 
 @pytest.mark.usefixtures("mock_account")
 def test_account_datetime(api_client):
