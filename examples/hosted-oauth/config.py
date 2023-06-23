211ac78c48f0ce2ed5ce5d2c158fe5d7c8ab0db0
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-06-22 22:50:50 -0400

    purge

--- examples/hosted-oauth/config.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = ("replace me with a random string",)
-NYLAS_OAUTH_CLIENT_ID = ("replace me with the client ID from Nylas",)
-NYLAS_OAUTH_CLIENT_SECRET = "replace me with the client secret from Nylas"
