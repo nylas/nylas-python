4b43b8c03f2540df65be6e6adbe2ee3eb4bacb84
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-06-23 14:18:55 -0400

    AV-1804 get availability support

--- nylas/resources/calendars.py
+++ nylas/resources/calendars.py
@@ -5,6 +5,7 @@ from nylas.handler.grants_api_resources import (
     UpdatableGrantsApiResource,
     DestroyableGrantsApiResource,
 )
+from nylas.model.response import Response
 
 
 class Calendars(
@@ -16,3 +17,20 @@ class Calendars(
 ):
     def __init__(self, http_client):
         super(Calendars, self).__init__("calendars", http_client)
+
+    def get_availability(self, identifier: str, request_body: dict) -> Response:
+        """Get availability for a calendar.
+
+        Args:
+            identifier (str): The grant ID or email account to get availability for.
+            request_body (dict): The request body to send to the API.
+
+        Returns:
+            Response: The availability response from the API.
+        """
+        json_response = self._http_client.post(
+            "/v3/grants/{}/calendar/availability".format(identifier),
+            request_body=request_body,
+        )
+
+        return Response.from_dict(json_response)
