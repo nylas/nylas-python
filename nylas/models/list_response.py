9568b35e46bbf88066cfc183ed61e80575d4e75c
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-07-07 16:41:44 -0400

    add tuple as superclass to responses
    allows us to return

--- nylas/models/list_response.py
+++ nylas/models/list_response.py
@@ -3,25 +3,28 @@ from typing import List, Optional, TypeVar, Generic, Type
 T = TypeVar("T")
 
 
-class ListResponse(Generic[T]):
+class ListResponse(tuple, Generic[T]):
     """List response object returned from the Nylas API."""
 
-    # request_id: str
-    # data: List[Dict[str, Any]]
-    # next_cursor: Optional[str]
+    data: List[T]
+    request_id: str
+    next_cursor: Optional[str] = None
 
-    def __init__(self, request_id: str, data: List[T], next_cursor: Optional[str]):
-        self.request_id = request_id
-        self.data = data
-        self.next_cursor = next_cursor
+    def __new__(cls, data: List[T], request_id: str, next_cursor: Optional[str] = None):
+        cls = tuple.__new__(cls, (data, request_id, next_cursor))
+        cls.data = data
+        cls.request_id = request_id
+        cls.next_cursor = next_cursor
+        return cls
 
     @classmethod
     def from_dict(cls, resp: dict, generic_type: Type[T]):
         converted_data = []
         for item in resp["data"]:
             converted_data.append(generic_type.from_dict(item))
+
         return cls(
-            request_id=resp["request_id"],
             data=converted_data,
+            request_id=resp["request_id"],
             next_cursor=resp.get("next_cursor", None),
         )
--- nylas/models/response.py
+++ nylas/models/response.py
@@ -5,19 +5,21 @@ from dataclasses_json import DataClassJsonMixin
 T = TypeVar("T", bound=DataClassJsonMixin)
 
 
-class Response(Generic[T]):
+class Response(tuple, Generic[T]):
     """Response object returned from the Nylas API."""
 
-    # request_id: str
-    # data: T
+    data: T
+    request_id: str
 
-    def __init__(self, request_id: str, data: T):
-        self.request_id = request_id
-        self.data = data
+    def __new__(cls, data: T, request_id: str):
+        cls = tuple.__new__(cls, (data, request_id))
+        cls.data = data
+        cls.request_id = request_id
+        return cls
 
     @classmethod
     def from_dict(cls, resp: dict, generic_type: Type[T]):
         return cls(
-            request_id=resp["request_id"],
             data=generic_type.from_dict(resp["data"]),
+            request_id=resp["request_id"],
         )
