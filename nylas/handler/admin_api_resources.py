5fc02ce3d6892f4c5789e99f0d7a8b5823ba0a30
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-07-10 11:09:29 -0400

    bring generics to admin api methods

--- nylas/handler/admin_api_resources.py
+++ nylas/handler/admin_api_resources.py
@@ -1,17 +1,22 @@
+from typing import TypeVar, Generic, get_args
+
 from nylas.models.delete_response import DeleteResponse
 from nylas.models.list_response import ListResponse
 from nylas.models.response import Response
 from nylas.resources.resource import Resource
 
+T = TypeVar("T")
 
-class ListableAdminApiResource(Resource):
+
+class ListableAdminApiResource(Resource, Generic[T]):
     def list(self, query_params: dict = None) -> ListResponse:
         path = "/v3/{}".format(self.resource_name)
         response_json = self._http_client.get(path, query_params)
-        return ListResponse.from_dict(response_json)
+        generic_type = get_args(self.__orig_bases__[0])[0]
+        return ListResponse.from_dict(response_json, generic_type)
 
 
-class FindableAdminApiResource(Resource):
+class FindableAdminApiResource(Resource, Generic[T]):
     def find(
         self,
         object_id: str,
@@ -19,19 +24,21 @@ class FindableAdminApiResource(Resource):
     ) -> Response:
         path = "/v3/{}/{}".format(self.resource_name, object_id)
         response_json = self._http_client.get(path, query_params=query_params)
-        return Response.from_dict(response_json)
+        generic_type = get_args(self.__orig_bases__[0])[0]
+        return Response.from_dict(response_json, generic_type)
 
 
-class CreatableAdminApiResource(Resource):
+class CreatableAdminApiResource(Resource, Generic[T]):
     def create(self, request_body: dict = None, query_params: dict = None) -> Response:
         path = "/v3/{}".format(self.resource_name)
         response_json = self._http_client.post(
             path, request_body=request_body, query_params=query_params
         )
-        return Response.from_dict(response_json)
+        generic_type = get_args(self.__orig_bases__[0])[0]
+        return Response.from_dict(response_json, generic_type)
 
 
-class UpdatableAdminApiResource(Resource):
+class UpdatableAdminApiResource(Resource, Generic[T]):
     def update(
         self,
         object_id: str,
@@ -42,7 +49,8 @@ class UpdatableAdminApiResource(Resource):
         response_json = self._http_client.put(
             path, request_body=request_body, query_params=query_params
         )
-        return Response.from_dict(response_json)
+        generic_type = get_args(self.__orig_bases__[0])[0]
+        return Response.from_dict(response_json, generic_type)
 
 
 class DestroyableAdminApiResource(Resource):
