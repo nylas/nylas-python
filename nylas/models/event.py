da2e31963ebeb814dea6b46920a7333fe33210b1
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-08-29 14:39:07 -0400

    AV-2307 Rest of the event models

--- nylas/models/event.py
+++ nylas/models/event.py
@@ -218,24 +218,321 @@ class Event:
     visibility: Optional[Visibility] = None
 
 
+class CreateParticipant(TypedDict):
+    """
+    Interface representing a participant for event creation.
+
+    Attributes:
+        email: Participant's email address.
+        name: Participant's name.
+        status: Participant's status.
+        comment: Comment by the participant.
+        phoneNumber: Participant's phone number.
+    """
+
+    email: str
+    status: NotRequired[ParticipantStatus]
+    name: NotRequired[str]
+    comment: NotRequired[str]
+    phoneNumber: NotRequired[str]
+
+
+class UpdateParticipant(TypedDict):
+    """
+    Interface representing a participant for updating an event.
+
+    Attributes:
+        email: Participant's email address.
+        name: Participant's name.
+        comment: Comment by the participant.
+        phoneNumber: Participant's phone number.
+    """
+
+    email: NotRequired[str]
+    name: NotRequired[str]
+    comment: NotRequired[str]
+    phoneNumber: NotRequired[str]
+
+
+class WritableDetailsConfig(TypedDict):
+    """
+    Interface representing a writable conferencing details config object
+
+    Attributes:
+        meeting_code: The conferencing meeting code. Used for Zoom.
+        password: The conferencing meeting password. Used for Zoom.
+        url: The conferencing meeting url.
+        pin: The conferencing meeting pin. Used for Google Meet.
+        phone: The conferencing meeting phone numbers. Used for Google Meet.
+    """
+
+    meeting_code: NotRequired[str]
+    password: NotRequired[str]
+    url: NotRequired[str]
+    pin: NotRequired[str]
+    phone: NotRequired[List[str]]
+
+
+class CreateDetails(TypedDict):
+    """
+    Interface representing a conferencing details object for event creation
+
+    Attributes:
+        provider: The conferencing provider
+        details: The conferencing details
+    """
+
+    provider: ConferencingProvider
+    details: WritableDetailsConfig
+
+
+class UpdateDetails(TypedDict):
+    """
+    Interface representing a conferencing details object for updating an event
+
+    Attributes:
+        provider: The conferencing provider
+        details: The conferencing details
+    """
+
+    provider: NotRequired[ConferencingProvider]
+    details: NotRequired[WritableDetailsConfig]
+
+
+class CreateAutocreate(TypedDict):
+    """
+    Interface representing a conferencing autocreate object for event creation
+
+    Attributes:
+        provider: The conferencing provider
+        autocreate: Empty dict to indicate an intention to autocreate a video link.
+            Additional provider settings may be included in autocreate.settings, but Nylas does not validate these.
+    """
+
+    provider: ConferencingProvider
+    autocreate: Dict[str, Any]
+
+
+class UpdateAutocreate(TypedDict):
+    """
+    Interface representing a conferencing autocreate object for event creation
+
+    Attributes:
+        provider: The conferencing provider
+        autocreate: Empty dict to indicate an intention to autocreate a video link.
+            Additional provider settings may be included in autocreate.settings, but Nylas does not validate these.
+    """
+
+    provider: NotRequired[ConferencingProvider]
+    autocreate: NotRequired[Dict[str, Any]]
+
+
+CreateConferencing = Union[CreateDetails, CreateAutocreate]
+UpdateConferencing = Union[UpdateDetails, UpdateAutocreate]
+
+
+# When
+class CreateTime(TypedDict):
+    """
+    Interface representing a specific point in time for event creation.
+    A meeting at 2pm would be represented as a time subobject.
+
+    Attributes:
+        time: A UNIX timestamp representing the time of occurrence.
+        timezone: If timezone is present, then the value for time will be read with timezone.
+            Timezone using IANA formatted string. (e.g. "America/New_York")
+    """
+
+    time: int
+    timezone: NotRequired[str]
+
+
+class UpdateTime(TypedDict):
+    """
+    Interface representing a specific point in time for updating an event.
+    A meeting at 2pm would be represented as a time subobject.
+
+    Attributes:
+        time: A UNIX timestamp representing the time of occurrence.
+        timezone: If timezone is present, then the value for time will be read with timezone.
+            Timezone using IANA formatted string. (e.g. "America/New_York")
+    """
+
+    time: NotRequired[int]
+    timezone: NotRequired[str]
+
+
+class CreateTimespan(TypedDict):
+    """
+    Interface representing a time span with start and end times for event creation.
+    An hour lunch meeting would be represented as timespan subobjects.
+
+    Attributes:
+        start_time: The start time of the event.
+        end_time: The end time of the event.
+        start_timezone: The timezone of the start time. Timezone using IANA formatted string. (e.g. "America/New_York")
+        end_timezone: The timezone of the end time. Timezone using IANA formatted string. (e.g. "America/New_York")
+    """
+
+    start_time: int
+    end_time: int
+    start_timezone: NotRequired[str]
+    end_timezone: NotRequired[str]
+
+
+class UpdateTimespan(TypedDict):
+    """
+    Interface representing a time span with start and end times for updating an event.
+    An hour lunch meeting would be represented as timespan subobjects.
+
+    Attributes:
+        start_time: The start time of the event.
+        end_time: The end time of the event.
+        start_timezone: The timezone of the start time. Timezone using IANA formatted string. (e.g. "America/New_York")
+        end_timezone: The timezone of the end time. Timezone using IANA formatted string. (e.g. "America/New_York")
+    """
+
+    start_time: NotRequired[int]
+    end_time: NotRequired[int]
+    start_timezone: NotRequired[str]
+    end_timezone: NotRequired[str]
+
+
+class CreateDate(TypedDict):
+    """
+    Interface representing an entire day spans without specific times for event creation.
+    Your birthday and holidays would be represented as date subobjects.
+
+    Attributes:
+        date: Date of occurrence in ISO 8601 format.
+    """
+
+    date: str
+
+
+class UpdateDate(TypedDict):
+    """
+    Interface representing an entire day spans without specific times for updating an event.
+    Your birthday and holidays would be represented as date subobjects.
+
+    Attributes:
+        date: Date of occurrence in ISO 8601 format.
+    """
+
+    date: NotRequired[str]
+
+
+class CreateDatespan(TypedDict):
+    """
+    Interface representing a specific dates without clock-based start or end times for event creation.
+    A business quarter or academic semester would be represented as datespan subobjects.
+
+    Attributes:
+        start_date: The start date in ISO 8601 format.
+        end_date: The end date in ISO 8601 format.
+    """
+
+    start_date: str
+    end_date: str
+
+
+class UpdateDatespan(TypedDict):
+    """
+    Interface representing a specific dates without clock-based start or end times for updating an event.
+    A business quarter or academic semester would be represented as datespan subobjects.
+
+    Attributes:
+        start_date: The start date in ISO 8601 format.
+        end_date: The end date in ISO 8601 format.
+    """
+
+    start_date: NotRequired[str]
+    end_date: NotRequired[str]
+
+
+CreateWhen = Union[CreateTime, CreateTimespan, CreateDate, CreateDatespan]
+UpdateWhen = Union[UpdateTime, UpdateTimespan, UpdateDate, UpdateDatespan]
+
+
 class CreateEventRequest(TypedDict):
     """
     Interface representing a request to create an event.
+
+    Attributes:
+        when: When the event occurs.
+        title: The title of the event.
+        busy: Whether the event is busy or free.
+        description: The description of the event.
+        location: The location of the event.
+        conferencing: The conferencing details of the event.
+        reminder_minutes: The number of minutes before the event start time when a user wants a reminder for this event.
+            Reminder minutes are in the following format: "[20]".
+        reminder_method: Method to remind the user about the event. (Google only).
+        metadata: Metadata associated with the event.
+        participants: The participants of the event.
+        recurrence: The recurrence rules of the event.
+        calendar_id: The ID of the calendar to create the event in.
+        read_only: Whether the event is read-only.
+        visibility: The visibility of the event.
+        capacity: The capacity of the event.
+        hide_participants: Whether to hide participants of the event.
     """
 
-    when: str
+    when: CreateWhen
     title: NotRequired[str]
     busy: NotRequired[bool]
     description: NotRequired[str]
     location: NotRequired[str]
-    conferencing: NotRequired[str]
-    reminderMinutes: NotRequired[str]
-    reminderMethod: NotRequired[str]
+    conferencing: NotRequired[CreateConferencing]
+    reminder_minutes: NotRequired[str]
+    reminder_method: NotRequired[str]
     metadata: NotRequired[Dict[str, Any]]
-    participants: NotRequired[List[Participant]]
+    participants: NotRequired[List[CreateParticipant]]
     recurrence: NotRequired[List[str]]
-    calendarId: NotRequired[str]
-    readOnly: NotRequired[bool]
-    visibility: NotRequired[str]
+    calendar_id: NotRequired[str]
+    read_only: NotRequired[bool]
+    visibility: NotRequired[Visibility]
     capacity: NotRequired[int]
-    hideParticipants: NotRequired[bool]
+    hide_participants: NotRequired[bool]
+
+
+class UpdateEventRequest(TypedDict):
+    """
+    Interface representing a request to update an event.
+
+    Attributes:
+        when: When the event occurs.
+        title: The title of the event.
+        busy: Whether the event is busy or free.
+        description: The description of the event.
+        location: The location of the event.
+        conferencing: The conferencing details of the event.
+        reminder_minutes: The number of minutes before the event start time when a user wants a reminder for this event.
+            Reminder minutes are in the following format: "[20]".
+        reminder_method: Method to remind the user about the event. (Google only).
+        metadata: Metadata associated with the event.
+        participants: The participants of the event.
+        recurrence: The recurrence rules of the event.
+        calendar_id: The ID of the calendar to create the event in.
+        read_only: Whether the event is read-only.
+        visibility: The visibility of the event.
+        capacity: The capacity of the event.
+        hide_participants: Whether to hide participants of the event.
+    """
+
+    when: NotRequired[UpdateWhen]
+    title: NotRequired[str]
+    busy: NotRequired[bool]
+    description: NotRequired[str]
+    location: NotRequired[str]
+    conferencing: NotRequired[UpdateConferencing]
+    reminder_minutes: NotRequired[str]
+    reminder_method: NotRequired[str]
+    metadata: NotRequired[Dict[str, Any]]
+    participants: NotRequired[List[UpdateParticipant]]
+    recurrence: NotRequired[List[str]]
+    calendar_id: NotRequired[str]
+    read_only: NotRequired[bool]
+    visibility: NotRequired[Visibility]
+    capacity: NotRequired[int]
+    hide_participants: NotRequired[bool]
