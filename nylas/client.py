c46edaa664dddb08ad80fd655613d10adde7c3db
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-08-30 11:14:18 -0400

    fix SDK entry point

--- nylas/client.py
+++ nylas/client.py
@@ -2,7 +2,6 @@ from nylas.config import DEFAULT_SERVER_URL
 from nylas.handler.http_client import HttpClient
 from nylas.resources.applications import Applications
 from nylas.resources.auth import Auth
-from nylas.resources.availability import Availability
 from nylas.resources.calendars import Calendars
 from nylas.resources.events import Events
 
@@ -17,16 +16,12 @@ class Client(object):
         self.api_server = api_server
         self.http_client = HttpClient(self.api_server, self.api_key, timeout)
 
-    def auth(self, client_id: str, client_secret: str) -> Auth:
-        return Auth(self.http_client, client_id, client_secret)
+    def auth(self) -> Auth:
+        return Auth(self.http_client)
 
     @property
     def applications(self) -> Applications:
         return Applications(self.http_client)
-
-    @property
-    def availability(self) -> Availability:
-        return Availability(self.http_client)
 
     @property
     def calendars(self) -> Calendars:
