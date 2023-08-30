7242909da5e418d39c03e635fb9e79def00e0ed7
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-08-30 17:09:28 -0400

    Refactor Auth classes (#267)
    This PR aligns the Python SDK to the other SDKs regarding Auth resource functions and models.

--- nylas/client.py
+++ nylas/client.py
@@ -2,31 +2,41 @@ from nylas.config import DEFAULT_SERVER_URL
 from nylas.handler.http_client import HttpClient
 from nylas.resources.applications import Applications
 from nylas.resources.auth import Auth
-from nylas.resources.availability import Availability
 from nylas.resources.calendars import Calendars
 from nylas.resources.events import Events
 
 
 class Client(object):
-    """API client for the Nylas API."""
+    """
+    API client for the Nylas API.
+
+    Attributes:
+        api_key: The Nylas API key to use for authentication
+        api_uri: The URL to use for communicating with the Nylas API
+        http_client: The HTTP client to use for requests to the Nylas API
+    """
 
     def __init__(
-        self, api_key: str, api_server: str = DEFAULT_SERVER_URL, timeout: int = 30
+        self, api_key: str, api_uri: str = DEFAULT_SERVER_URL, timeout: int = 30
     ):
-        self.api_key = api_key
-        self.api_server = api_server
-        self.http_client = HttpClient(self.api_server, self.api_key, timeout)
+        """
+        Initialize the Nylas API client.
 
-    def auth(self, client_id: str, client_secret: str) -> Auth:
-        return Auth(self.http_client, client_id, client_secret)
+        Args:
+            api_key: The Nylas API key to use for authentication
+            api_uri: The URL to use for communicating with the Nylas API
+            timeout: The timeout for requests to the Nylas API, in seconds
+        """
+        self.api_key = api_key
+        self.api_uri = api_uri
+        self.http_client = HttpClient(self.api_uri, self.api_key, timeout)
+
+    def auth(self) -> Auth:
+        return Auth(self.http_client)
 
     @property
     def applications(self) -> Applications:
         return Applications(self.http_client)
-
-    @property
-    def availability(self) -> Availability:
-        return Availability(self.http_client)
 
     @property
     def calendars(self) -> Calendars:
--- nylas/resources/auth.py
+++ nylas/resources/auth.py
@@ -8,7 +8,12 @@ from nylas.models.auth import (
     CodeExchangeResponse,
     PkceAuthUrl,
     OpenID,
-    ServerSideHostedAuthResponse,
+    CodeExchangeRequest,
+    TokenExchangeRequest,
+    ProviderDetectResponse,
+    ProviderDetectParams,
+    URLForAuthenticationConfig,
+    URLForAdminConsentConfig,
 )
 from nylas.models.response import Response
 from nylas.resources.grants import Grants
@@ -20,74 +25,103 @@ def _hash_pkce_secret(secret: str) -> str:
     return base64.b64encode(sha256_hash).decode()
 
 
+def _build_query(config: dict) -> dict:
+    config["response_type"] = "code"
+
+    if not config["access_type"]:
+        config["access_type"] = "online"
+
+    if config["scopes"]:
+        config["scopes"] = " ".join(config["scopes"])
+
+    return config
+
+
+def _build_query_with_pkce(config: dict, secret_hash: str) -> dict:
+    params = _build_query(config)
+
+    params["code_challenge"] = secret_hash
+    params["code_challenge_method"] = "s256"
+
+    return params
+
+
+def _build_query_with_admin_consent(config: dict) -> dict:
+    params = _build_query(config)
+
+    params["response_type"] = "adminconsent"
+    params["credential_id"] = config["credentialId"]
+
+    return params
+
+
 class Auth(Resource):
-    def __init__(self, http_client: HttpClient, client_id: str, client_secret: str):
+    def __init__(self, http_client: HttpClient):
         super(Auth, self).__init__(http_client)
-        self.client_id = client_id
-        self.client_secret = client_secret
 
     @property
     def grants(self) -> Grants:
         return Grants(self._http_client)
 
-    def exchange_code_for_token(
-        self, code: str, redirect_uri: str, code_verifier: str = None
-    ) -> Response[CodeExchangeResponse]:
-        """Exchange an authorization code for an access token.
+    def url_for_oauth_2(self, config: URLForAuthenticationConfig) -> str:
+        """
+        Build the URL for authenticating users to your application via Hosted Authentication.
 
         Args:
-            code (str): The OAuth 2.0 code from the authorization request.
-            redirect_uri (str): The redirect URI of the integration.
-            code_verifier (str): The code verifier used to generate the code challenge.
+            config: The configuration for building the URL.
 
         Returns:
-            Response: The API response with the access token information.
+            The URL for hosted authentication.
+        """
+        query = _build_query(config)
+
+        return self._url_auth_builder(query)
+
+    def exchange_code_for_token(
+        self, request: CodeExchangeRequest
+    ) -> Response[CodeExchangeResponse]:
+        """
+        Exchange an authorization code for an access token.
+
+        Args:
+            request: The request parameters for the code exchange
+
+        Returns:
+            Information about the Nylas application
         """
 
-        request_body = {
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "grant_type": "authorization_code",
-            "code": code,
-            "redirect_uri": redirect_uri,
-        }
-
-        if code_verifier is not None:
-            request_body["code_verifier"] = code_verifier
+        request_body = dict(request)
+        request_body["grant_type"] = "authorization_code"
 
         return self._get_token(request_body)
 
     def refresh_access_token(
-        self, refresh_token: str, redirect_uri: str
+        self, request: TokenExchangeRequest
     ) -> Response[CodeExchangeResponse]:
-        """Refresh an access token.
+        """
+        Refresh an access token.
 
         Args:
-            refresh_token (str): The refresh token to refresh.
-            redirect_uri (str): The redirect URI of the integration.
+            request: The refresh token request.
 
         Returns:
-            Response: The API response with the refreshed token information.
+            The response containing the new access token.
         """
 
-        request_body = {
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "grant_type": "refresh_token",
-            "code": refresh_token,
-            "redirect_uri": redirect_uri,
-        }
+        request_body = dict(request)
+        request_body["grant_type"] = "refresh_token"
 
         return self._get_token(request_body)
 
     def validate_id_token(self, id_token: str) -> Response[OpenID]:
-        """Validate an ID token.
+        """
+        Validate an ID token.
 
         Args:
-            id_token (str): The ID token to validate.
+            id_token: The ID token to validate.
 
         Returns:
-            Response: The API response with the token information.
+            The API response with the token information.
         """
 
         query_params = {
@@ -97,13 +131,14 @@ class Auth(Resource):
         return self._validate_token(query_params)
 
     def validate_access_token(self, access_token: str) -> Response[OpenID]:
-        """Validate an access token.
+        """
+        Validate an access token.
 
         Args:
-            access_token (str): The access token to validate.
+            access_token: The access token to validate.
 
         Returns:
-            Response: The API response with the token information.
+            The API response with the token information.
         """
 
         query_params = {
@@ -112,80 +147,43 @@ class Auth(Resource):
 
         return self._validate_token(query_params)
 
-    def url_for_authentication(self, config: dict) -> str:
-        """Build the URL for authenticating users to your application via Hosted Authentication.
-
-        Args:
-            config (dict): The configuration for the authentication request.
-
-        Returns:
-            str: The URL for hosted authentication.
+    def url_for_oauth_2_pkce(self, config: URLForAuthenticationConfig) -> PkceAuthUrl:
         """
-        query = self._build_query(config)
-
-        return self._url_auth_builder(query)
-
-    def url_for_authentication_pkce(self, config: dict) -> PkceAuthUrl:
-        """Build the URL for authenticating users to your application via Hosted Authentication with PKCE.
+        Build the URL for authenticating users to your application via Hosted Authentication with PKCE.
 
         IMPORTANT: YOU WILL NEED TO STORE THE 'secret' returned to use it inside the CodeExchange flow
 
         Args:
-            config (dict): The configuration for the authentication request.
+            config: The configuration for the authentication request.
 
         Returns:
-            PkceAuthUrl: The URL for hosted authentication with secret & hashed secret.
+            The URL for hosted authentication with secret & hashed secret.
         """
         secret = str(uuid.uuid4())
         secret_hash = _hash_pkce_secret(secret)
-        query = self._build_query_with_pkce(config, secret_hash)
+        query = _build_query_with_pkce(config, secret_hash)
 
         return PkceAuthUrl(secret, secret_hash, self._url_auth_builder(query))
 
-    def url_for_admin_consent(self, config: dict) -> str:
+    def url_for_admin_consent(self, config: URLForAdminConsentConfig) -> str:
         """Build the URL for admin consent authentication for Microsoft.
 
         Args:
-            config (dict): The configuration for the authentication request.
+            config: The configuration for the authentication request.
 
         Returns:
-            str: The URL for hosted authentication.
+            The URL for hosted authentication.
         """
         config_with_provider = {"provider": "microsoft", **config}
-        query = self._build_query_with_admin_consent(config_with_provider)
+        query = _build_query_with_admin_consent(config_with_provider)
 
         return self._url_auth_builder(query)
-
-    def server_side_hosted_auth(
-        self, config: dict
-    ) -> Response[ServerSideHostedAuthResponse]:
-        """Create a new authorization request and get a new unique login url.
-        Used only for hosted authentication.
-        This is the initial step requested from the server side to issue a new login url.
-
-        Args:
-            config (dict): The configuration for the authentication request.
-
-        Returns:
-            Response: The API response with the authorization request.
-        """
-        credentials = "{}:{}".format(self.client_id, self.client_secret)
-        encoded_credentials = base64.b64encode(credentials.encode()).decode("utf-8")
-
-        json_response = self._http_client._execute(
-            method="POST",
-            path="/v3/connect/auth",
-            request_body=config,
-            headers={"Authorization": "Basic {}".format(encoded_credentials)},
-        )
-
-        return Response.from_dict(json_response, ServerSideHostedAuthResponse)
 
     def revoke(self, token: str) -> True:
         """Revoke a single access token.
 
         Args:
-            token (str): The access token to revoke.
+            token: The access token to revoke.
 
         Returns:
             True: If the token was revoked successfully.
@@ -198,56 +196,30 @@ class Auth(Resource):
 
         return True
 
+    def detect_provider(
+        self, params: ProviderDetectParams
+    ) -> Response[ProviderDetectResponse]:
+        """
+        Detect provider from email address.
+
+        Args:
+            params: The parameters to include in the request
+
+        Returns:
+            The detected provider, if found.
+        """
+
+        json_response = self._http_client._execute(
+            method="POST",
+            path="/v3/providers/detect",
+            query_params=params,
+        )
+        return Response.from_dict(json_response, ProviderDetectResponse)
+
     def _url_auth_builder(self, query: dict) -> str:
         return "{}/v3/connect/auth?{}".format(
             self._http_client.api_server, urllib.parse.urlencode(query)
         )
-
-    def _build_query(self, config: dict) -> dict:
-        params = {
-            "client_id": self.client_id,
-            "redirect_uri": config["redirect_uri"],
-            "access_type": config["accessType"] or "offline",
-            "provider": config["provider"],
-            "response_type": "code",
-        }
-
-        if config["loginHint"]:
-            params["login_hint"] = config["loginHint"]
-            if config["includeGrantScopes"]:
-                params["include_grant_scopes"] = str(
-                    config["includeGrantScopes"]
-                ).lower()
-
-        if config["scopes"]:
-            params["scopes"] = " ".join(config["scopes"])
-
-        if config["prompt"]:
-            params["prompt"] = config["prompt"]
-
-        if config["metadata"]:
-            params["metadata"] = config["metadata"]
-
-        if config["state"]:
-            params["state"] = config["state"]
-
-        return params
-
-    def _build_query_with_pkce(self, config: dict, secret_hash: str) -> dict:
-        params = self._build_query(config)
-
-        params["code_challenge"] = secret_hash
-        params["code_challenge_method"] = "s256"
-
-        return params
-
-    def _build_query_with_admin_consent(self, config: dict) -> dict:
-        params = self._build_query(config)
-
-        params["response_type"] = "adminconsent"
-        params["credential_id"] = config["credentialId"]
-
-        return params
 
     def _get_token(self, request_body: dict) -> Response[CodeExchangeResponse]:
         json_response = self._http_client._execute(
