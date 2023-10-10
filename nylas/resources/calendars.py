de94c2762192ed5ab36f6499077d79fcbf84beab
Christine Spang <christine@spang.cc>
2023-10-10 14:34:08 -0700

    Add support for free-busy endpoint (#279)
    * Add support for free-busy endpoint
    
    Mostly cribbed off the availability support, though radically stripped
    down as free-busy is a great deal simpler.
    
    * debugging
    
    * just work
    
    * fix docs
    
    * fix doc
    
    * rename Error to FreeBusyError

--- nylas/resources/calendars.py
+++ nylas/resources/calendars.py
@@ -6,6 +6,7 @@ from nylas.handler.api_resources import (
     DestroyableApiResource,
 )
 from nylas.models.availability import GetAvailabilityResponse, GetAvailabilityRequest
+from nylas.models.free_busy import GetFreeBusyResponse, GetFreeBusyRequest
 from nylas.models.calendars import (
     Calendar,
     CreateCalendarRequest,
@@ -132,3 +133,24 @@ class Calendars(
         )
 
         return Response.from_dict(json_response, GetAvailabilityResponse)
+
+    def get_free_busy(
+        self, identifier: str, request_body: GetFreeBusyRequest
+    ) -> Response[GetFreeBusyResponse]:
+        """
+        Get free/busy info for a Calendar.
+
+        Args:
+            identifier: The grant ID or email account to get free/busy for.
+            request_body: The request body to send to the API.
+
+        Returns:
+            Response: The free/busy response from the API.
+        """
+        json_response = self._http_client._execute(
+            method="POST",
+            path=f"/v3/grants/{identifier}/calendars/free-busy",
+            request_body=request_body,
+        )
+
+        return Response(json_response, GetFreeBusyResponse)
