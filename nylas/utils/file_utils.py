7bbaf0d23e3d743f290d7b8b47bbfd31c190a50a
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2023-11-22 15:53:31 -0500

    Add helper for attaching files to request

--- nylas/utils/file_utils.py
+++ nylas/utils/file_utils.py
@@ -1,8 +1,35 @@
 import json
 import mimetypes
 import os
+from pathlib import Path
 
 from requests_toolbelt import MultipartEncoder
+
+from nylas.models.attachments import CreateAttachmentRequest
+
+
+def attach_file_request_builder(file_path) -> CreateAttachmentRequest:
+    """
+    Build a request to attach a file.
+
+    Attributes:
+        file_path: The path to the file to attach.
+
+    Returns:
+        A properly-formatted request to attach the file.
+    """
+    path = Path(file_path)
+    filename = path.name
+    size = os.path.getsize(file_path)
+    content_type = mimetypes.guess_type(file_path)[0]
+    file_stream = open(file_path, "rb")
+
+    return {
+        "filename": filename,
+        "content_type": content_type if content_type else "application/octet-stream",
+        "content": file_stream,
+        "size": size,
+    }
 
 
 def _build_form_request(request_body: dict) -> MultipartEncoder:
