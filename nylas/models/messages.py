6aa554dc1db7659356d07358338783f1f34611f0
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2024-01-29 15:56:36 +0400

    fix date type

--- nylas/models/messages.py
+++ nylas/models/messages.py
@@ -58,7 +58,6 @@ class Message:
 
     grant_id: str
     from_: List[EmailName] = field(metadata=config(field_name="from"))
-    date: int
     object: str = "message"
     id: Optional[str] = None
     body: Optional[str] = None
@@ -75,7 +74,7 @@ class Message:
     unread: Optional[bool] = None
     starred: Optional[bool] = None
     created_at: Optional[int] = None
-    date: Optional[datetime] = None
+    date: Optional[int] = None
 
 
 # Need to use Functional typed dicts because "from" and "in" are Python
