a6331bc850f27ba0bc086cbef65b47f9ab2410c1
Mostafa Rashed <17770919+mrashed-dev@users.noreply.github.com>
2024-03-04 20:46:11 -0500

    lint

--- examples/native-authentication-gmail/server.py
+++ examples/native-authentication-gmail/server.py
@@ -90,6 +90,7 @@ app.register_blueprint(google_bp, url_prefix="/login")
 # Teach Flask how to find out that it's behind an ngrok proxy
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
+
 # Define what Flask should do when someone visits the root URL of this website.
 @app.route("/")
 def index():
